4c3ef2e0dc09866fcab81a2f5d9191db0523369b
pokerchen <1418045110@qq.com>
2019-07-18 18:08:57 +0800

    FangYuanDao接口

---
+++ src/main/java/site/ccczg/dao/FangYuanDao.java
@@ -0,0 +1,18 @@
+package site.ccczg.dao;
+
+import site.ccczg.domain.FangYuanCar;
+/**
+ * @Author: Effort
+ * @Date: 2019/7/18 14:12
+ * @Description: FangYuanDao接口
+ */
+public interface FangYuanDao {
+    /**
+     * @Author 陈治国
+     * @Description 添加车辆
+     * @Date 2019/7/18 17:18
+     * @Param [fangYuanCar]
+     * @return void
+     **/
+    void add(FangYuanCar fangYuanCar);
+}
